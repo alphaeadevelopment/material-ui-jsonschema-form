eaaeaf7c328a3218795698ac54562e3b39d28a94
Graham King <gking2224@gmail.com>
2018-04-10 16:34:03 +0100

    Reboot -> CssBaseline

--- src/demo/MuiRoot.jsx
+++ src/demo/MuiRoot.jsx
@@ -1,13 +1,13 @@
 import React from 'react';
 import 'typeface-roboto'; // eslint-disable-line import/extensions
-import Reboot from 'material-ui/Reboot';
+import CssBaseline from 'material-ui/CssBaseline';
 
 import { MuiThemeProvider } from 'material-ui/styles';
 import theme from './theme';
 
 export default ({ children }) => (
   <MuiThemeProvider theme={theme}>
-    <Reboot />
+    <CssBaseline />
     {children}
   </MuiThemeProvider>
 );
